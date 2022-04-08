3116e27d665909401f41c347a320275acd46913c
Giovanni_Francesco_Giovene <gianfri.gvn@gmail.com>
2022-04-08 14:12:25 +0200

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,37 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container-fluid">
+    <HeaderComponent id="header" class=""/>
+    <MainComponent id="main"/>
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderComponent from './components/HeaderComponent.vue'
+import MainComponent from './components/MainComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HeaderComponent,
+    MainComponent,
   }
 }
 </script>
 
 <style lang="scss">
+  @import 'bootstrap/scss/bootstrap';
+  @import '@/style/variables';
+
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  #header{
+    background-color: $bg-header-color;
+    
+  }
+  #main{}
 }
 </style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="row align-items-center">   
+        LOGO
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+
+<style lang="scss" scoped>
+@import '@/style/variables';
+
+    div{
+        color: #fff;
+        height: 80px;
+    }
+</style>
